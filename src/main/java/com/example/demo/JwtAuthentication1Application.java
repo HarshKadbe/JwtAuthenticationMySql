41a3aef932cce6e9d00032a3352ce53d13f075d7
HarshKadbe <harshkadbe44@gmail.com>
2024-06-15 19:43:26 +0530

    first commit

---
+++ src/main/java/com/example/demo/JwtAuthentication1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthentication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthentication1Application.class, args);
+	}
+
+}
